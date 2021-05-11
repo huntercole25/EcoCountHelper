a6c5f5e4bc900bfcebc92fdf5dfee52290827930
huntercole25 <53839906+huntercole25@users.noreply.github.com>
2021-05-10 20:02:28 -0700

    Adding devtools install line to vignette and updating ResidPlot functions

--- R/ResidPlotLong.R
+++ R/ResidPlotLong.R
@@ -3,7 +3,7 @@
 #' @export
 #' 
 ResidPlotLong <- function (Data, CountCol, GroupCol, ModNames, GroupPat = "^[[:alnum:]]+", 
-                           Looped = T, Nsims = 1000, TestVals = T) 
+Looped = T, Nsims = 1000, TestVals = T) 
 {
   ResidPlotterSub <- function(GroupMod) {
     TmpFile1 <- paste0(tempfile(), ".png")
@@ -19,30 +19,12 @@ ResidPlotLong <- function (Data, CountCol, GroupCol, ModNames, GroupPat = "^[[:a
     ModTab <- stats::simulate(TmpMod, nsim = Nsims)
     SimMod <- do.call(cbind, ModTab)
     SimModDharma <- DHARMa::createDHARMa(simulatedResponse = SimMod, 
-                                         observedResponse = TmpFull[[CountCol]], fittedPredictedResponse = stats::predict(TmpMod), 
+                                         observedResponse = TmpFull[[CountCol]], fittedPredictedResponse = predict(TmpMod), 
                                          integerResponse = TRUE)
-    GroupResidTests <- DHARMa::testResiduals(SimModDharma, plot = F)
-    grDevices::png(TmpFile1)
-    graphics::par(mfrow = c(1,2))
-    DHARMa::plotQQunif(SimModDharma, testUniformity = F, testOutliers = F, testDispersion = F)
-    graphics::title(sub = GroupMod, font.sub = 2)
-    DHARMa::testDispersion(SimModDharma)
-    grDevices::dev.off()
     
-    grDevices::png(TmpFile2)
-    graphics::par(mfrow = c(1,1))
-    DHARMa::testOutliers(SimModDharma)
-    grDevices::dev.off()
-    
-    Plot1 <- png::readPNG(TmpFile1)
-    Plot2 <- png::readPNG(TmpFile2)
-    
-    Plot1Gr <- grid::rasterGrob(Plot1)
-    Plot2Gr <- grid::rasterGrob(Plot2)
-    
-    gridExtra::grid.arrange(Plot1Gr, Plot2Gr, nrow = 1)
-    
+    GroupResidTests <- DHARMa::testResiduals(SimModDharma, plot = T)
     ResidPlot <- grDevices::recordPlot()
+    
     assign(paste0(GroupMod, "SimResidPlot"), ResidPlot, 
            pos = .GlobalEnv)
     if (TestVals == T) {
